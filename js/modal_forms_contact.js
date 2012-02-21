804e318893cc17e5090073b3716b9a06fa384005
Geraldo Ramos <gramos1984@gmail.com>
2012-02-21 14:03:19 -0500

    check if contact url has already been overwritten

--- js/modal_forms_contact.js
+++ js/modal_forms_contact.js
@@ -1,5 +1,7 @@
 Drupal.behaviors.initModalFormsContact = function (context) {
   $("a[href*='/contact'], a[href*='?q=contact']", context).each(function () {
-    this.href = this.href.replace(/contact/, 'modal_forms/nojs/contact');
+    if(!$(this).hasClass('init-modal-forms-contact')){
+      this.href = this.href.replace(/contact/, 'modal_forms/nojs/contact');
+    }
   }).addClass('init-modal-forms-contact ctools-use-modal ctools-modal-modal-popup-medium');
 }
